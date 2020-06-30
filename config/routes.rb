f4b0be5c26b4bac17f30d2d404a880bd2152e307
Greg Byrne <greg@showoff.ie>
2020-06-30 23:10:28 +0100

    rails + react router

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
       resources :lists
     end
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+  
 end
