406baa0cc9ae42808c74f78ef29054710c866c2a
Greg Byrne <greg@showoff.ie>
2020-06-30 22:46:49 +0100

    app refactor + heroku config

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -6,6 +6,7 @@ module Api::V1
     # GET /lists.json
     def index
       @lists = List.all
+      render :json => @lists
     end
 
     # GET /lists/1
@@ -27,28 +28,20 @@ module Api::V1
     def create
       @list = List.new(list_params)
 
-      respond_to do |format|
-        if @list.save
-          format.html { redirect_to @list, notice: 'List was successfully created.' }
-          format.json { render :show, status: :created, location: @list }
-        else
-          format.html { render :new }
-          format.json { render json: @list.errors, status: :unprocessable_entity }
-        end
+      if @list.save
+        render json: @list, status: :created
+      else
+        render json: @list.errors, status: :unprocessable_entity
       end
     end
 
     # PATCH/PUT /lists/1
     # PATCH/PUT /lists/1.json
     def update
-      respond_to do |format|
-        if @list.update(list_params)
-          format.html { redirect_to @list, notice: 'List was successfully updated.' }
-          format.json { render :show, status: :ok, location: @list }
-        else
-          format.html { render :edit }
-          format.json { render json: @list.errors, status: :unprocessable_entity }
-        end
+      if @list.update(list_params)
+        render json: @list
+      else
+        render json: @list.errors, status: :unprocessable_entity
       end
     end
 
@@ -56,9 +49,10 @@ module Api::V1
     # DELETE /lists/1.json
     def destroy
       @list.destroy
-      respond_to do |format|
-        format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
-        format.json { head :no_content }
+      if @list.destroy
+        head :no_content, status: :ok
+      else
+        render json: @list.errors, status: :unprocessable_entity
       end
     end
 
