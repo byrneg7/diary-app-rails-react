454174d348a3425b431a705eb57d334cacf1095e
byrneg7 <greg@showoff.ie>
2020-07-12 22:05:00 +0100

    prod config

---
+++ public/precache-manifest.f60ac2f8a9de4475dc1ba27b6e773449.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59283fc7777ad175eaf77bc8994752af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ee5ad76fa1efa8add5b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "125463f14564004f2bdb",
+    "url": "/static/css/main.d82fb118.chunk.css"
+  },
+  {
+    "revision": "5ee5ad76fa1efa8add5b",
+    "url": "/static/js/2.70df7da4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.70df7da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125463f14564004f2bdb",
+    "url": "/static/js/main.5443a87b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "52c7f87989e5ef36fa378dcdbc96bd3a",
+    "url": "/static/media/blog-1.52c7f879.jpg"
+  },
+  {
+    "revision": "bff794763ee171aba1a4bd6e72b93fa7",
+    "url": "/static/media/blog-2.bff79476.jpg"
+  },
+  {
+    "revision": "3b90dc4de567ad9b5f2e8a789ec0e329",
+    "url": "/static/media/blog-3.3b90dc4d.jpg"
+  },
+  {
+    "revision": "7bab806144a291dd43768e4a780d8ba8",
+    "url": "/static/media/cover_img_1.7bab8061.jpg"
+  },
+  {
+    "revision": "eb296bd8b33c0ed028b558819d9ffaee",
+    "url": "/static/media/dashboard_full_1.eb296bd8.jpg"
+  },
+  {
+    "revision": "2e70d8e3df0ec7d7cdbcf969a17d3161",
+    "url": "/static/media/icomoon.2e70d8e3.eot"
+  },
+  {
+    "revision": "b14756d795c50f428fe3a63daf8b09c1",
+    "url": "/static/media/icomoon.b14756d7.ttf"
+  },
+  {
+    "revision": "e06462b34b492af7a0b359ad61f792e3",
+    "url": "/static/media/icomoon.e06462b3.svg"
+  },
+  {
+    "revision": "fa282d6c576ceb9e8fe8752b94f6aebf",
+    "url": "/static/media/icomoon.fa282d6c.woff"
+  },
+  {
+    "revision": "7b52357ad7fb0b20310120d61c5d59ab",
+    "url": "/static/media/mobile-1.7b52357a.jpg"
+  },
+  {
+    "revision": "c8fc678b0131bb9f024bfab858801a8c",
+    "url": "/static/media/mobile-2.c8fc678b.jpg"
+  }
+]);
