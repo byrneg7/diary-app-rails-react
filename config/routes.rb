762522b7bf4d3819fb37260d5d2a9718c48cd069
Greg Byrne <greg@showoff.ie>
2020-07-12 20:58:54 +0100

    routing for react

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
       resources :users, only: [:create, :show, :index]
     end
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+  
 end
