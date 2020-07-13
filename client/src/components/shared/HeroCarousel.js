3ffdcb6a1ca5307b188936aeadb776164b69a8fb
byrneg7 <greg@showoff.ie>
2020-07-13 21:23:11 +0100

    work on styling + refactor

--- client/src/components/shared/HeroCarousel.js
+++ client/src/components/shared/HeroCarousel.js
@@ -3,15 +3,13 @@ import React from 'react';
 import dashboardBoard1 from '../../assets/images/dashboard_full_1.jpg';
 
 const HeroCarousel = () => {
-  return(
+  return (
     <div className="colorlib-featured">
-      <div className="row animate-box">
-        <div className="featured-wrap">
-          <div className="item">
-            <div className="col-md-8 col-md-offset-2">
-              <div className="featured-entry">
-                <img className="img-responsive" src={dashboardBoard1} alt=""/>
-              </div>
+      <div className="featured-wrap">
+        <div className="item">
+          <div className="col-md-8 col-md-offset-2">
+            <div className="featured-entry">
+              <img className="img-responsive" src={dashboardBoard1} alt=""/>
             </div>
           </div>
         </div>
